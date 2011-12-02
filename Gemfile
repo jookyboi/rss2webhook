867090f5c15d924af14f0444551f1d78ab8250e5
Rui Jiang <rui@yesware.com>
2011-12-02 10:01:29 -0500

    Started on rails app.

---
+++ Gemfile
@@ -0,0 +1,28 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0'
+
+# Infrastructure
+gem 'pg'
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'coffee-rails', "~> 3.1.0"
+  gem 'uglifier'
+end
+
+gem 'sass-rails', "  ~> 3.1.0"
+gem 'jquery-rails'
+
+group :development, :test do
+  gem 'annotate'
+  gem 'rspec-rails'
+  gem 'foreman'
+  gem 'heroku'
+end
+
+group :test do
+  # Pretty printed test output
+  gem 'turn', :require => false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rss2webhook::Application.config.secret_token = '971786c8ba03b4e57003f6770dab2e3d9d2565a6575ed91c64a5b77c4385a8316bdfa0f8ad1d399a1f949a7ab248be7a911b0f960bd269d5eee6ae999834b23e'
