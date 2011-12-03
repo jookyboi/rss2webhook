b4973436f9991674e0be86ad63bc4f99bc0ef13e
Rui Jiang <rui@yesware.com>
2011-12-03 17:21:00 -0500

    process_new_articles_job able to save new articles.

--- lib/delayed_jobs/process_new_articles_job.rb
+++ lib/delayed_jobs/process_new_articles_job.rb
@@ -10,9 +10,28 @@ class ProcessNewArticlesJob < Struct.new(:rss_feed, :settings)
 
     feed = RSS::Parser.parse(content, false)
 
-    # assume that items with different URLs are different
+    # all articles previously fetched
+    feed_articles = Article.where(:feed_url => feed.channel.link)
+    first_fetch = !feed_articles.any?
+
+    # insert unique items into mongo
     feed.items.each do |item|
-      
+      item_hash = Hash.from_xml(item.to_s)['item']
+      item_hash['feed_url'] = feed.channel.link
+
+      unless feed_articles.where(:link => item_hash['link']).any?
+        puts item_hash['link']
+
+        # assume that items with different URLs are different
+        article = Article.new(item_hash)
+        article.save!
+      end
     end
+
+    schedule_next(settings['check_interval'])
+  end
+
+  def schedule_next(check_interval)
+    Delayed::Job.enqueue self, 0, Time.now + check_interval
   end
 end
