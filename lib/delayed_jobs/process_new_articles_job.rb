b479705490d75b1859a829a825cbde93d728a8b5
Rui Jiang <rui@yesware.com>
2011-12-03 17:58:45 -0500

    Added scheduling of future tasks.

--- lib/delayed_jobs/process_new_articles_job.rb
+++ lib/delayed_jobs/process_new_articles_job.rb
@@ -16,19 +16,32 @@ class ProcessNewArticlesJob < Struct.new(:rss_feed, :settings)
 
     # insert unique items into mongo
     feed.items.each do |item|
+      
       item_hash = Hash.from_xml(item.to_s)['item']
       item_hash['feed_url'] = feed.channel.link
 
       unless feed_articles.where(:link => item_hash['link']).any?
-        puts item_hash['link']
+
+        if !first_fetch || (first_fetch && settings['process_on_start'])
+          post_to_webhook(item_hash.to_json, rss_feed['webhook'])
+        end
 
         # assume that items with different URLs are different
         article = Article.new(item_hash)
         article.save!
       end
+      
     end
 
     schedule_next(settings['check_interval'])
+  end
+
+  def post_to_webhook(item_json, webhook_url)
+    begin
+      response = RestClient.post webhook_url, :article => item_json
+    rescue => e
+      e.response
+    end
   end
 
   def schedule_next(check_interval)
