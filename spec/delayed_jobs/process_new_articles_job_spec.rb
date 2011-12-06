ae82785f0175013ad98168693930df0fd074ba48
Rui Jiang <rui@yesware.com>
2011-12-05 21:58:23 -0500

    Augmented unauthenticated rss test.

--- spec/delayed_jobs/process_new_articles_job_spec.rb
+++ spec/delayed_jobs/process_new_articles_job_spec.rb
@@ -5,6 +5,10 @@ describe ProcessNewArticlesJob do
     Article.all.each do |a|
       a.delete
     end
+
+    Delayed::Job.all.each do |job|
+      job.delete
+    end
   end
 
   it 'should process simple unauthenticated rss' do
@@ -17,6 +21,16 @@ describe ProcessNewArticlesJob do
     item = Article.where(:link => 'http://labs.silverorange.com/archives/2003/june/canyousaythat')
     item.any?.should be(true)
     item.size.should be(1)
+
+    # ensure all the properties are on the article
+    article = item.first
+    article.description.should_not be(nil)
+    article.description.should_not be ""
+
+    article.title.should_not be(nil)
+    article.link.should match('http://labs.silverorange.com/archives/2003/june/canyousaythat')
+    article.guid.should_not be(nil)
+    article.comments.should_not be(nil)
 
     Article.all.size.should be(6)
   end
